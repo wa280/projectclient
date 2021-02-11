42aaecb9d9b8f78672ab9903dab750bdd3b00eea
wahta <chigoveratinoonga@gmail.com>
2021-02-12 00:29:05 +0200

    edited proxy and server base URL

--- src/Apolloprovider.js
+++ src/Apolloprovider.js
@@ -7,7 +7,7 @@ import{ApolloProvider} from '@apollo/react-hooks'
 import{setContext} from 'apollo-link-context'
 
 const httpLink = createHttpLink({
-    uri:'http://localhost:5000'
+    uri:'https://lit-thicket-32155.herokuapp.com/'
 });
 const authLink = setContext(()=>{
     const token = localStorage.getItem('jwtToken')
